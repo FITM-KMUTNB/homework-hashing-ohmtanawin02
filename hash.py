f4d1c27a6fecfb2d85938356f954fc4101779445
Tanawin Jenkunnaphat <againnakub@hotmail.com>
2020-01-12 22:51:29 +0700

    com

--- hash.py
+++ hash.py
@@ -1,4 +1,5 @@
 information = ["","","","","","","","","",""]
+again = "Y"
 def list(key,value,number):
     global information
     data = {key,value}
@@ -13,8 +14,39 @@ def list(key,value,number):
                 break
     return information
 
-def hash_key(ke):
-    index_num = hash(ke)%10
+def hash_key(key):
+    index_num = hash(key)%10
     if index_num >= 10 :
         index_num = index_num % 10
     return index_num
+while again == "Y":
+    print("1.INSERT")
+    print("2.SEARCH")
+    print("3.EXIT")
+    enter = input("Enter choice : ")
+    if enter == "1":
+        one = "Y"
+        while one == "Y":
+            if "" not in information:
+                print("Full")
+                one = "N"
+            key = input("Enter Name : ")
+            value = input("Enter id : ")
+            number = hash_key(key)
+            list(key,value,number)
+            one = input("More input : Y=yes ,N = Negative : ")
+    elif enter == "2" :
+        two = "Y"
+        while two == "Y":
+            key = input('Enter name : ')
+            number = hash_key(key)
+            if key in information[number]:
+                print("Your ID : ",information[number].get(key))
+            else:
+                for i in information:
+                    if key in i :
+                        print("your ID : ",i.get(key))
+                        break
+            two = input("More input : Y=yes ,N = Negative : ")
+    else:
+        again = "N"
